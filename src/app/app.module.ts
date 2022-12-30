e57b54d0e1ecf16e4e726b8c526b5deade56f677
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-30 19:22:04 +0530

    viewall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudententryComponent } from './studententry/studententry.component';
+import { ViewallComponent } from './viewall/viewall.component';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:StudententryComponent
+  },
+  {
+    path:"view",
+    component:ViewallComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    StudententryComponent
+    StudententryComponent,
+    ViewallComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
