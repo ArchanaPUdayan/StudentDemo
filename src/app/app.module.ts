154a0c9d60df73087fd0f08b20ccf2b9f23fd804
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-30 19:46:08 +0530

    viewAllpa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudententryComponent } from './studententry/studententry.component';
+import { ViewAllpaComponent } from './view-allpa/view-allpa.component';
+import { SearchComponent } from './search/search.component';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:StudententryComponent
+  },
+  {
+    path:"view",
+    component:ViewAllpaComponent
+  },
+  {
+    path:"search",
+    component: SearchComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    StudententryComponent
+    StudententryComponent,
+    ViewAllpaComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/view-allpa/view-allpa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-allpa',
+  templateUrl: './view-allpa.component.html',
+  styleUrls: ['./view-allpa.component.css']
+})
+export class ViewAllpaComponent {
+
+}
